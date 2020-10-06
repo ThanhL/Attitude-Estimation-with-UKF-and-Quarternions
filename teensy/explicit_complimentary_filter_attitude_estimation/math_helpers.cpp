f9584fd5afa6d3b7f8786d5ed29ffde274172271
unknown <thanhl354@gmail.com>
2020-10-06 20:35:10 +1100

    added skew matrix math using BasicLinearAlgebra arduino. Seems like the route we  be taking...

--- teensy/explicit_complimentary_filter_attitude_estimation/math_helpers.cpp
+++ teensy/explicit_complimentary_filter_attitude_estimation/math_helpers.cpp
@@ -3,6 +3,7 @@ Math helpers implementation
 */
 #include "Arduino.h"
 #include "math_helpers.h"
+#include <BasicLinearAlgebra.h>
 
 
 float constrain_angle(float x)
@@ -14,10 +15,24 @@ float constrain_angle(float x)
 }
 
 
-
-BLA::Matrix<4,4> skew_matrix(BLA::Matrix<4,4> x)
+BLA::Matrix<4,4> skew_matrix_4d(BLA::Matrix<4> x)
 {
 	BLA::Matrix<4,4> mat;
-	mat.Fill(1);
+	mat << 0, -x(0), x(1), -x(2),
+		x(0), 0, x(2), x(3),
+		-x(1), -x(2), 0, -x(0),
+		x(2), -x(3), x(0), 0;
 	return mat;
 }
+
+
+BLA::Matrix<3,3> skew_matrix_3d(BLA::Matrix<3> x)
+{
+	BLA::Matrix<3,3> mat;
+	mat << 0.0, -x(2), x(1),
+		x(2), 0.0, -x(0),
+		-x(1), x(0), 0.0;
+	return mat;
+}
+
+
--- teensy/explicit_complimentary_filter_attitude_estimation/math_helpers.h
+++ teensy/explicit_complimentary_filter_attitude_estimation/math_helpers.h
@@ -4,8 +4,10 @@ Math helpers header file
 #include "Arduino.h"
 #include "BasicLinearAlgebra.h"
 
+
 float constrain_angle(float x);
-BLA::Matrix<4,4> skew_matrix(BLA::Matrix<4,4> x); 
+BLA::Matrix<4,4> skew_matrix_4d(BLA::Matrix<4> x); 
+BLA::Matrix<3,3> skew_matrix_3d(BLA::Matrix<3> x);
 
 
 
